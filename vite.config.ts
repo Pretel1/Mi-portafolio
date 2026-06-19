15406494fe9a0969ac9bdbf03ad877fea118feb2
Dany Jose <danyjose709@gmail.com>
2026-06-19 17:49:49 -0500

    ci: trigger deployment workflow after configuring settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  // Configuración para el despliegue en GitHub Pages
   base: '/Mi-portafolio/',
   plugins: [
     react(),
